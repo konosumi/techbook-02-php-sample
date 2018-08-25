e2c59f9394f8439411ea73008150fc9ad4a4f721
konosumi <36559308+konosumi@users.noreply.github.com>
2018-08-26 01:09:32 +0900

    commit

--- public/index.php
+++ public/index.php
@@ -1,33 +1,41 @@
 <?php
-
 // ルーティング設定
-$routingPath = [
-    '/chapter01/c01',
-    '/chapter01/c02',
-    '/chapter01/c03/c03',
-    '/chapter01/c04',
-    '/chapter02/c01',
-    '/chapter02/c02',
-    '/chapter02/c03',
-    '/chapter02/c04',
-    '/chapter02/c05',
-    '/chapter03/c01',
-    '/chapter03/c02',
-    '/chapter03/c03',
-    '/chapter04/c01',
-    '/chapter04/c02',
-    '/chapter04/c03',
-    '/chapter04/c04',
-];
-// Like a ['/chapter01/01' => '/chapter01/01']
-$routeConfig = array_combine($routingPath, $routingPath);
-
+// キーと値が同じ配列を作る => ['/chapter01/01' => '/chapter01/01']
+$routeConfig = array_combine(routingPath(), routingPath());
 $uri = $_SERVER['REQUEST_URI'];
-// ルーティング設定に搭載された、正規のURIのみルーティングを実行します
-if (isset($routeConfig[$uri])) {
-    // 実際のファイルパスは「/var/www/sample/phpsamples/chapter01/03.php」です。
-    require_once(__DIR__.'/../phpsamples'.$routeConfig[$uri].'.php');
+
+// ルーティング設定に搭載された、正規のURI以外を弾きます。
+if (!isset($routeConfig[$uri])) {
+    header("HTTP/1.0 404 Not Found");
     return;
 }
 
-header("HTTP/1.0 404 Not Found");
+// "/var/www/sample/phpsamples/phpinfo.php"のようなパスになります。
+//var_dump(realpath(__DIR__.'/../phpsamples'.$routeConfig[$uri].'.php'));
+
+// phpsamplesの下にある、パスに対応したPHPファイルを呼び出します。
+// ルーティングエンジンのリストに載っているパスに限定されているため、安全に呼び出すことができます。
+require(__DIR__.'/../phpsamples'.$routeConfig[$uri].'.php');
+
+// アクセスを受け付けるパスの一覧を返す
+function routingPath() {
+    return [
+        '/phpinfo',
+        '/chapter01/c01',
+        '/chapter01/c02',
+        '/chapter01/c03/c03',
+        '/chapter01/c04',
+        '/chapter02/c01',
+        '/chapter02/c02',
+        '/chapter02/c03',
+        '/chapter02/c04',
+        '/chapter02/c05',
+        '/chapter03/c01',
+        '/chapter03/c02', 
+        '/chapter03/c03',
+        '/chapter04/c01',
+        '/chapter04/c02',
+        '/chapter04/c03',
+        '/chapter04/c04',
+    ];
+}
