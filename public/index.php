5e8833f93140033521fb9178a34a8568a0f3e6f0
konosumi <36559308+konosumi@users.noreply.github.com>
2018-08-26 09:25:55 +0900

    commit sampl

--- public/index.php
+++ public/index.php
@@ -1,22 +1,21 @@
 <?php
-// ルーティング設定
-// array_combineは、キーと値が同じ配列を作っているだけです。
+// ルーティング設定の作成(キーと値が同じ配列を作ってます)
 $routeConfig = array_combine(routingPath(), routingPath());
+// ルーティングはURI(URL)を基に行ないます。
 $uri = $_SERVER['REQUEST_URI'];
 
-// ルーティング設定に搭載された、正規のURL(URI)以外を弾きます。
+// ルーティング設定にない、正規のURI(URL)以外を弾きます。
 if (!isset($routeConfig[$uri])) {
     header("HTTP/1.0 404 Not Found");
     return;
 }
 
 // phpsamplesの下にある、パスに対応したPHPファイルを呼び出します。
-// 呼び出しは、ルーティングエンジンに掲載されているパス(PHPファイル)に限定します。
 $path = realpath(__DIR__.'/../phpsamples'.$routeConfig[$uri].'.php');
 if ($path) {
     require($path);
 } else {
-    // ルーティングを適切にメンテナンスしていれば、ここにくることはない
+    // ルーティングを適切にメンテナンスしていれば、ここには来ない
     header("HTTP/1.0 500 Internal Server Error");
     return;
 }
