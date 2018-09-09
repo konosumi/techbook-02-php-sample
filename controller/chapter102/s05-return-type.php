da947036d73fe1dee28b2e53087a53c774db3171
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-09 10:32:02 +0900

    戻り値の型宣言

---
+++ controller/chapter102/s05-return-type.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+// 戻り値の型宣言
+final class SampleReturnType
+{
+    public static function addNumberReturn(int $number): ?int
+    {
+        // 正常に動く
+        if ($number === 1) {
+            return null;
+        }
+        // 正常に動く
+        if ($number === 2) {
+            return $number + 1;
+        }
+
+        // 戻り値が数値でないのでエラーになる
+        return false;
+    }
+}
+
+// 何も表示されない(NULLが表示される)
+echo SampleReturnType::addNumberReturn(1)."\n<br>";
+// 3が表示される
+echo SampleReturnType::addNumberReturn(2)."\n<br>";
+// Fatal error: Uncaught TypeError: Return value of
+// SampleReturnType::addNumberReturn()
+// must be of the type integer or null, boolean returned in 
+echo SampleReturnType::addNumberReturn(3)."\n<br>";
--- public/router.php
+++ public/router.php
@@ -31,6 +31,7 @@ function routingPath()
         '/chapter102/s02-instance-or-null',
         '/chapter102/s03-weak',
         '/chapter102/s04-strong',
+        '/chapter102/s05-return-type',
         // 変数のスコープ
         '/chapter103/s01-local-scope',
         /*
