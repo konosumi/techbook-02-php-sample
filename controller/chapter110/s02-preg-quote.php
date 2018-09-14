81cc41ef4b6899121e0dcb70c002535c063876de
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-15 03:44:11 +0900

    コミット

--- controller/chapter110/s02-preg-quote.php
+++ controller/chapter110/s02-preg-quote.php
@@ -5,19 +5,22 @@
 // 正規表現を想定していない只の文字列とする
 $str = 'aa*b*cc';
 // "a*b*c"という只の文字列を"d"にする想定です
-[$original, $replace] = ["a*b*c","d"];
+[$word, $replace] = ["a*b*c","d"];
 
 // なぜかaa*b*ddになってしまう
 // 理由は、アスタリスク(*)が正規表現におけるメタ文字だからである
 // 結果： aa*b*dd
-echo htmlspecialchars(preg_replace('/'.$original.'/', $replace, $str), ENT_QUOTES)."\n<br>";
+echo htmlspecialchars(
+    preg_replace('/'.$word.'/', $replace, $str), ENT_QUOTES)."\n<br>";
 
 // 本ケースでにおける"a*b*c"は「0文字以上のaの連続,0文字以上のbの連続,最後にc」となる
 // つまり、c単体の一文字でもヒットします
 // 結果： d
-echo htmlspecialchars(preg_replace('/'.$original.'/', $replace, 'c'), ENT_QUOTES)."\n<br>";
+echo htmlspecialchars(
+    preg_replace('/'.$word.'/', $replace, 'c'), ENT_QUOTES)."\n<br>";
 
 // メタ文字のアスタリスク(*)を、単純な文字として判定したい場合、
-// クォートという処理でエスケープすれば可能でです
+// preg_quoteという処理でエスケープすれば可能でです
 // 結果：adc
-echo htmlspecialchars(preg_replace('/'.preg_quote($original, '/').'/', $replace, $str), ENT_QUOTES)."\n<br>";
+echo htmlspecialchars(
+    preg_replace('/'.preg_quote($word, '/').'/', $replace, $str), ENT_QUOTES)."\n<br>";
