ba6f176a63e94c30f61d0b31d853e7a7cf12b541
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-17 15:40:39 +0900

    kommit

--- public/router.php
+++ public/router.php
@@ -1,85 +1,24 @@
 <?php
-// ルーティング設定の作成(キーと値が同じ配列を作るだけです)
-$routerConfig = array_combine(routingPath(), routingPath());
+// ルーティング設定
+$routerConfig = [
+    '/phpinfo' => '/phpinfo'
+];
 // ルーティングはURIを基に判定します
 $uri = $_SERVER['REQUEST_URI'];
 
-// ルーティング設定にない、不正なURIを弾きます。
+// ルーティング設定に存在しない不正なURIを弾きます。
 if (!isset($routerConfig[$uri])) {
     header("HTTP/1.0 404 Not Found");
     return;
 }
 
 // controllerの下にある、パスに対応したPHPファイルを呼び出します。
-$contBasePath = __DIR__.'/../controller';
-$contPath = realpath($contBasePath.$routerConfig[$uri].'.php');
-if ($contPath) {
-    require($contPath);
+$basePath = __DIR__.'/../controller';
+$ctrlPath = realpath($basePath.$routerConfig[$uri].'.php');
+if ($ctrlPath) {
+    require($ctrlPath);
 } else {
-    // パス一覧をメンテナンスしていれば、来ることはない
+    // ルーティング設定をメンテナンスしていれば、来ることはない
     header("HTTP/1.0 500 Internal Server Error");
     return;
-}
-
-// ルーティングで受け付けるパス一覧を返す
-function routingPath()
-{
-    return [
-        '/phpinfo',
-        // 型宣言
-        '/chapter102/s01-basic',
-        '/chapter102/s02-instance-or-null',
-        '/chapter102/s03-weak',
-        '/chapter102/s04-strong',
-        '/chapter102/s05-return-type',
-        '/chapter102/s06-try-catch',
-        // 変数のスコープ
-        '/chapter103/s01-local-scope',
-        // 新しい演算子
-        '/chapter104/s01-null-operator',
-        '/chapter104/s02-elvis-operator',
-        '/chapter104/s03-compare-operator',
-        '/chapter104/s04-const-array',
-        '/chapter104/s05-anonymous-class',
-        '/chapter104/s06-list-array',
-        '/chapter104/s07-assert',
-        // 名前空間
-        '/chapter105/s01-declare-error',
-        '/chapter105/s02-namespace',
-        '/chapter105/s03/deep-namespace',
-        '/chapter105/s04-use',
-        '/chapter105/s05-function-define',
-        // オートロード
-        '/chapter106/s01-autoload',
-        '/chapter106/s02-auto-autoload',
-        // コンポーザー
-        '/chapter107/s01-naoki-hanzawa',
-        // 正規表現
-        '/chapter110/s01-preg-match-delimiter',
-        '/chapter110/s02-preg-quote',
-        '/chapter110/s03-preg-match-multi',
-        '/chapter110/s04-preg-match-small-wide',
-        '/chapter110/s05-preg-match-speed',
-        '/chapter110/s06-preg-match-jp',
-        // 配列
-        '/chapter111/s01-array-merge',
-        '/chapter111/s02-array-mix',
-        '/chapter111/s03-no-name-function',
-        // PHPの判定と比較
-        '/chapter112/s01-if-bool-condition',
-        '/chapter112/s02-if-string-zero',
-        '/chapter112/s03-switch-condition',
-        '/chapter112/s04-int-cast-condition',
-        '/chapter112/s05-strict-compare',
-        '/chapter112/s06-strict-in-array',
-        '/chapter112/s07-float-compare',
-        // エラーと例外
-        '/chapter113/s01-error-exception-basic',
-        '/chapter113/s02-exception-basic',
-        '/chapter113/s03-exception-handler',
-        '/chapter113/s04-set-error-handler',
-        '/chapter113/s05-debug-print-backtrace',
-        // テンプレートエンジン
-        '/chapter116/s01-smarty',
-    ];
 }
