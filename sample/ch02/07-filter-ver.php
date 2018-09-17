fbbf1da0fc8b334fd307177920594f30d61e42f0
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-17 20:59:16 +0900

    00

--- sample/ch02/07-filter-ver.php
+++ sample/ch02/07-filter-ver.php
@@ -3,6 +3,7 @@ $var = filter_var('755x', FILTER_VALIDATE_INT);
 // bool(false)
 var_dump($var);
 
+// 文字列の'755'が数値の755になる
 $var = filter_var('755', FILTER_VALIDATE_INT);
 // int(755)
 var_dump($var);
