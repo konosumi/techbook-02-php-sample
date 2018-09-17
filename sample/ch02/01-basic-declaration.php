c6a0e9e968ecf7eab9dacaf9b72124d0b68e0b12
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-17 18:13:13 +0900

    サンプルの修正

---
+++ sample/ch02/01-basic-declaration.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 型宣言の基本的なサンプル
+ */
+final class SampleTypeDeclaration
+{
+    // 今までの書き方
+    public static function echoUserId($userId)
+    {
+        echo "userId=".$userId.PHP_EOL;
+    }
+
+    // 型宣言によって、引数が整数である事を保証する
+    public static function echoUserIdType(int $userId)
+    {
+        echo "userId=".$userId.PHP_EOL;
+    }
+}
+
+$sampleUserId = 'abcde';
+
+// userId=abcde
+SampleTypeDeclaration::echoUserId($sampleUserId);
+
+// userId=12345
+SampleTypeDeclaration::echoUserIdType(12345);
+
+/**
+ * 整数を要求している関数に対して、文字列を与えてしまった。
+ * Fatal error: Uncaught TypeError: Argument 1 passed to
+ * SampleTypeDeclaration::echoUserIdType()
+ * must be of the type integer, string given, called in 
+ */
+SampleTypeDeclaration::echoUserIdType($sampleUserId);
