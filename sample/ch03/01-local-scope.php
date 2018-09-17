c7939c0df89b73e619f2112b4408ef3350c59285
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-18 01:54:04 +0900

    ローカルスコープ

--- sample/ch03/01-local-scope.php
+++ sample/ch03/01-local-scope.php
@@ -1,5 +1,7 @@
 <?php
-// ローカルスコープ
+/**
+ * ローカルスコープ
+ */
 function localscope()
 {
     // 関数内で宣言された変数は、関数内でしか生存しません。
@@ -7,6 +9,8 @@ function localscope()
     return $str;
 }
     
+// 関数を呼び出してみる
 localscope();
+
 // str is not alive
-echo isset($str) ? "str is alive." : "str is not alive.";
+echo isset($str) ? 'str is alive.' : 'str is not alive.';
