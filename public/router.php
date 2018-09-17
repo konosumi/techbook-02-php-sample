06610e844011fda10768a232387b5fae98775a08
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-17 15:52:10 +0900

    kommit

--- public/router.php
+++ public/router.php
@@ -3,18 +3,20 @@
 $routerConfig = [
     '/phpinfo' => '/phpinfo'
 ];
-// ルーティングはURIを基に判定します
-$uri = $_SERVER['REQUEST_URI'];
+
+// ルーティングはURIのパスを基に判定します
+// 単純に使うと?クエリで誤判定するので、パスだけ抜き出します
+$uriPath = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 // ルーティング設定に存在しない不正なURIを弾きます。
-if (!isset($routerConfig[$uri])) {
+if (!isset($routerConfig[$uriPath])) {
     header("HTTP/1.0 404 Not Found");
     return;
 }
 
 // controllerの下にある、パスに対応したPHPファイルを呼び出します。
 $basePath = __DIR__.'/../controller';
-$ctrlPath = realpath($basePath.$routerConfig[$uri].'.php');
+$ctrlPath = realpath($basePath.$routerConfig[$uriPath].'.php');
 if ($ctrlPath) {
     require($ctrlPath);
 } else {
