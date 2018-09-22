54215fab2cd97b29d4be68741538f4bf33e8ea8a
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 01:41:22 +0900

    fix sample

--- sample/ch06/01-autoload.php
+++ sample/ch06/01-autoload.php
@@ -6,15 +6,17 @@
 // まだ読み込みまれていないクラスや関数を使おうとした時に、
 // 実行される関数を登録する
 spl_autoload_register(function ($classname) {
-    echo '新しいクラス(関数)を読み込みます: '.$classname.PHP_EOL;
+    echo '新しく読み込みます: '.$classname.PHP_EOL;
     $ds = DIRECTORY_SEPARATOR;
 
-    // クラスの名前空間を含めた住所と、実際のファイルの場所を対応させる
+    // クラスの名前空間を含めた住所と、実際のファイルを対応させます
     $autoloadConfig = [
-        'sample\ch06\WelcomeAutoload' => __DIR__.$ds.'WelcomeAutoload.php'
+        'sample\ch06\WelcomeAutoload' =>
+            __DIR__.$ds.'WelcomeAutoload.php'
     ];
 
-    // クラスが定義されているPHPファイルを読み込むことで、使えるようにします
+    // クラスが定義されているPHPファイルを読み込みます
+    // これで使えるようにします
     if (isset($autoloadConfig[$classname])) {
         include($autoloadConfig[$classname]);
     }
@@ -22,7 +24,7 @@ spl_autoload_register(function ($classname) {
 
 /**
  * 初回の呼び出しだけ、クラスの読み込みが走っていることが分かります
- * 新しいクラス(関数)を読み込みます: sample\ch06\WelcomeAutoload
+ * 新しく読み込みます: sample\ch06\WelcomeAutoload
  * Welcome to autoload!
  * Welcome to autoload!
  */ 
