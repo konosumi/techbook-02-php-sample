21069ec4b5068926f24a568bf6d4f7ac13ced764
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-22 17:30:17 +0900

    完成

--- sample/ch04/01-null-coalescing-operator.php
+++ sample/ch04/01-null-coalescing-operator.php
@@ -3,6 +3,15 @@
  * Null合体演算子
  */
 
+// 普通に書くと
+if (isset($_GET['user'])) {
+    $username = $_GET['user'];
+} else {
+    $username = 'nobody';
+}
+// またはこうなる
+$username = isset($_GET['user']) ? $_GET['user'] : 'nobody';
+
 // Null合体演算子(??)は、isset相当の判定を行ないます
 $username = $_GET['user'] ?? 'nobody';
 // nobody
