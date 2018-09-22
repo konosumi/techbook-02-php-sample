5ff372abb274f6d0876f6eed6ef026cf472cf698
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 01:33:34 +0900

    o

--- old/chapter106/Sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace chapter106;
-
-class Sample {
-    function hello() {
-        echo "Welcome to autoload!\n<br>";
-    }
-}
---
+++ sample/ch06/01-autoload.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * オートロードの基本形
+ */
+
+// まだ読み込みまれていないクラスや関数を使おうとした時に、
+// 実行される関数を登録する
+spl_autoload_register(function ($classname) {
+    echo '新しいクラス(関数)を読み込みます: '.$classname.PHP_EOL;
+    $ds = DIRECTORY_SEPARATOR;
+
+    // クラスの名前空間を含めた住所と、実際のファイルの場所を対応させる
+    $autoloadConfig = [
+        'sample\ch06\WelcomeAutoload' => __DIR__.$ds.'WelcomeAutoload.php'
+    ];
+
+    // クラスが定義されているPHPファイルを読み込むことで、使えるようにします
+    if (isset($autoloadConfig[$classname])) {
+        include($autoloadConfig[$classname]);
+    }
+});
+
+/**
+ * 初回の呼び出しだけ、クラスの読み込みが走っていることが分かります
+ * 新しいクラス(関数)を読み込みます: sample\ch06\WelcomeAutoload
+ * Welcome to autoload!
+ * Welcome to autoload!
+ */ 
+$sample = new \sample\ch06\WelcomeAutoload();
+$sample->welcome();
+$sample2 = new \sample\ch06\WelcomeAutoload();
+$sample2->welcome();
