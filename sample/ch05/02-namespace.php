2fcb4c77c75f4fd6b98d68b416b7f85b696be37b
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-22 23:13:25 +0900

    名前空間の利用

---
+++ sample/ch05/02-namespace.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 名前空間の利用
+ */
+
+ /**
+  * 名前空間を宣言します。これから定義するクラス(や関数・定数)に、住所を与えます
+  * 名前空間は階層化できます。住所で例えるならば「sample県ch05市」のようなものです
+  */
+namespace sample\ch05;
+
+// \sample\ch05\DateTimeというクラスパスになります
+final class DateTime {
+    function __construct()
+    {
+    }
+
+    public function example()
+    {
+        return "My DateTime Class";
+    }
+}
+
+// どちらの書き方でも動きます
+// 1. 現在自分がいる名前空間で定義されているクラスを使う(相対)
+$datetime = new DateTime();
+echo $datetime->example().PHP_EOL;
+// 2. 名前空間をはじめから記述する(絶対)
+$datetime = new \sample\ch05\DateTime();
+echo $datetime->example().PHP_EOL;
+
+// 本来のPHPのDateTimeを使う際は、先頭にバックスラッシュを付けます
+$original = new \DateTime();
+echo $original->format('Y-m-d H:i:s').PHP_EOL;
