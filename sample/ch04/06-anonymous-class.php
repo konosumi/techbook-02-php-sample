1e6a85ad24fa72c9616e5c2beda7c4339c9f8e15
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-22 21:12:38 +0900

    -

--- sample/ch04/06-anonymous-class.php
+++ sample/ch04/06-anonymous-class.php
@@ -6,37 +6,37 @@ class BaseBatchController
 {
     public function __construct()
     {
-        echo 'BaseBatchController: __construct()'.PHP_EOL;
+        echo 'BaseBatchController __construct()'.PHP_EOL;
     }
 
     public function run()
     {
-        echo 'BaseBatchController: run()'.PHP_EOL;
+        echo 'BaseBatchController run()'.PHP_EOL;
     }
 }
 
 /**
  * 無名クラスによる即席バッチコントローラー(基盤クラスを継承しています)
- * BaseBatchController: __construct()
- * AnonymousController: __construct()
+ * BaseBatchController __construct()
+ * AnonymousController __construct()
  */
 $controller = new class extends BaseBatchController
 {
     public function __construct()
     {
         parent::__construct();
-        echo 'AnonymousController: __construct()'.PHP_EOL;
+        echo 'AnonymousController __construct()'.PHP_EOL;
     }
 
     public function run()
     {
         parent::run();
-        echo 'AnonymousController: run()'.PHP_EOL;
+        echo 'AnonymousController run()'.PHP_EOL;
     }
 };
 
-// BaseBatchController: run()
-// AnonymousController: run()
+// BaseBatchController run()
+// AnonymousController run()
 $controller->run();
 
 // オブジェクトのクラス名を取得すると、とても長い名前が返却されます
