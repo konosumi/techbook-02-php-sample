8f843abc2f6463371c0b850c7a43b33fa28531e1
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-24 00:36:15 +0900

    -

---
+++ sample/ch12/07-float-compare.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * floatの比較が失敗するケース
+ * 参考： WEB+DB PRESS Vol.94「PHP初心者がハマりがちな落とし穴」
+ */
+$float1 = 1.1;
+$float2 = 2.2;
+
+// bool(false) bool(false)
+var_dump(3.3 == ($float1 + $float2));
+var_dump(3.3 === ($float1 + $float2));
+
+// 一般的に、プログラムにおいて少数の扱いは難しいです
+// 例えば、1/3は割り切れないので、表現には限界があります
+// float(0.33333333333333)
+var_dump(1/3);
