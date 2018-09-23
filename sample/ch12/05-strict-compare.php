8814f8a0953d7ced7931ef08f866f5153213fa6c
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-24 00:19:12 +0900

    -

--- sample/ch12/05-strict-compare.php
+++ sample/ch12/05-strict-compare.php
@@ -1,21 +1,19 @@
 <?php
+/**
+ * 厳密な比較
+ */
 // bool(true) bool(false) 
 var_dump(0 == 'A'); 
 var_dump(0 === 'A'); 
-echo "<br>";
 
 // bool(true) bool(false) 
 var_dump(0 == '0'); 
 var_dump(0 === '0'); 
-echo "<br>";
 
 // bool(true) bool(false) 
 var_dump(0 == false); 
 var_dump(0 === false); 
-echo "<br>";
 
 // bool(true) bool(false) 
 var_dump(0 == 0); 
 var_dump(0 === 0); 
-echo "<br>";
-
--- sample/ch12/06-strict-in-array.php
+++ sample/ch12/06-strict-in-array.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * in_arrayにおける緩やかな比較と厳密な比較
+ */
 $value = 0;
 
+// 緩やかな比較
 // bool(true)
 var_dump(in_array($value, ['A', 'B', 'C']));
-echo "\n<br>";
 
+// 厳密な比較
 // bool(false)
 var_dump(in_array($value, ['A', 'B', 'C'], true));                              
-echo "\n<br>";
