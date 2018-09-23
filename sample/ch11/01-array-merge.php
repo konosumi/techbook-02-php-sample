438dec54859c5ea7f44700af6ba0f64a6283c4fb
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 21:12:18 +0900

    配列の結合

--- sample/ch11/01-array-merge.php
+++ sample/ch11/01-array-merge.php
@@ -1,33 +1,40 @@
 <?php
+/**
+ * 配列の結合
+ */
+
+/**
+ * 数値配列のケース
+ */
 $array1 = [1, 2];
 $array2 = [3, 4, 5];
 
-// プラスでの配列結合は、+の左側が優先され、左側がなければ右側を使用する
-// 結果：array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(5) } 
+// プラスでの配列結合は、左側が優先されます
+// 結果： array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(5) }
 var_dump($array1 + $array2);
-echo "<br>";
 
-// array_mergeでは、数値配列は単純に左側の末尾に要素として追加される
+// array_mergeでは、数値配列は末尾に要素として追加されます
 // array(5) {
 //   [0]=> int(1) [1]=> int(2)
 //   [2]=> int(3) [3]=> int(4) [4]=> int(5)
 // }
 var_dump(array_merge($array1, $array2));
-echo "<br>";
 
+/**
+ * 連想配列のケース
+ */
 $array3 = ['fruits' => 'apple', 'code' => 'php'];
 $array4 = ['fruits' => 'oprange', 'programing' => 'perl'];
 
-// fruitsは被っているので、左側が使用される
+// fruitsは重複していますが、左側が優先されます
 // array(3) {
 //   ["fruits"]=> string(5) "apple"
 //   ["code"]=> string(3) "php"
 //   ["programing"]=> string(4) "perl"
 // } 
 var_dump($array3 + $array4);
-echo "<br>";
 
-// fruitsは被っているので、右側が使用される
+// fruitsは重複していますが、右側が優先されます
 // array(3) {
 //   ["fruits"]=> string(7) "oprange"
 //   ["code"]=> string(3) "php"
