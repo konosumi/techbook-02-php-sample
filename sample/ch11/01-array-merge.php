b5d9cf184bca7fa06e104370390a0ff8787a9963
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 21:23:59 +0900

    配列の結合

--- sample/ch11/01-array-merge.php
+++ sample/ch11/01-array-merge.php
@@ -1,37 +1,28 @@
 <?php
 /**
- * 配列の結合
+ * 配列の結合(数値配列)
  */
-
-// 数値配列のケース
 $array1 = [1, 2];
 $array2 = [3, 4, 5];
 
 // プラスでの配列結合は、左側が優先されます
-// 結果： array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(5) }
+// 結果： [[0]=> 1 [1]=> 2 [2]=> 5]
 var_dump($array1 + $array2);
 
 // array_mergeでは、数値配列は末尾に要素として追加されます
-// array(5) { [0]=> int(1) [1]=> int(2)
-//   [2]=> int(3) [3]=> int(4) [4]=> int(5) }
+// 結果： [[0]=> 1 [1]=> 2 [2]=> 3 [3]=> 4 [4]=> 5]
 var_dump(array_merge($array1, $array2));
 
-// 連想配列のケース
+/**
+ * 配列の結合(連想配列)
+ */
 $array3 = ['fruits' => 'apple', 'code' => 'php'];
-$array4 = ['fruits' => 'oprange', 'programing' => 'perl'];
+$array4 = ['fruits' => 'oprange', 'drink' => 'beer'];
 
 // fruitsは重複していますが、左側が優先されます
-// array(3) {
-//   ["fruits"]=> string(5) "apple"
-//   ["code"]=> string(3) "php"
-//   ["programing"]=> string(4) "perl"
-// } 
+// 結果： [["fruits"]=> "apple" ["code"]=> "php" ["drink"]=> "beer"]
 var_dump($array3 + $array4);
 
 // fruitsは重複していますが、右側が優先されます
-// array(3) {
-//   ["fruits"]=> string(7) "oprange"
-//   ["code"]=> string(3) "php"
-//   ["programing"]=> string(4) "perl"
-// }
+// 結果： [["fruits"]=> "oprange" ["code"]=> "php" ["drink"]=> "beer"]
 var_dump(array_merge($array3, $array4));
