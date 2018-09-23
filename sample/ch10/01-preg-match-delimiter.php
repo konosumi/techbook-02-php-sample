adf4bacc0a25187c219d369ff2634741ff471967
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 17:21:41 +0900

    正規表現のデリミタ

--- sample/ch10/01-preg-match-delimiter.php
+++ sample/ch10/01-preg-match-delimiter.php
@@ -4,12 +4,12 @@
  */
 
 // 正規表現のデリミタはスラッシュが多いです
-var_dump(preg_match('/^123/', '12345'));
+preg_match('/^123/', '12345');
 
 // デリミタは英数字、バックスラッシュ、空白文字以外の任意の文字が可能です
-var_dump(preg_match('@^123@', '12345'));
-var_dump(preg_match('%^123%', '12345'));
+preg_match('@^123@', '12345');
+preg_match('%^123%', '12345');
 
 // よく使われるデリミタは、スラッシュ(/)、ハッシュ記号(#)、チルダ(~)です。
-var_dump(preg_match('#^/path#', '/path/to/test'));
-var_dump(preg_match('~^/path~', '/path/to/test'));
+preg_match('#^/path#', '/path/to/test');
+preg_match('~^/path~', '/path/to/test');
