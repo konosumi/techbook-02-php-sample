4fd7e4786e21c08c99f930250e900c98d484f122
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 17:17:39 +0900

    正規表現のデリミタ

--- sample/ch10/01-preg-match-delimiter.php
+++ sample/ch10/01-preg-match-delimiter.php
@@ -1,14 +1,15 @@
 <?php
-// 以下の例は、いずれもマッチします。
-// SEE: http://php.net/manual/ja/regexp.reference.delimiters.php
+/**
+ * 正規表現のデリミタ
+ */
 
 // 正規表現のデリミタはスラッシュが多いです
-var_dump(preg_match('/^1/', '12345'));
+var_dump(preg_match('/^123/', '12345'));
 
 // デリミタは英数字、バックスラッシュ、空白文字以外の任意の文字が可能です
-var_dump(preg_match('@^1@', '12345'));
+var_dump(preg_match('@^123@', '12345'));
+var_dump(preg_match('%^123%', '12345'));
 
 // よく使われるデリミタは、スラッシュ(/)、ハッシュ記号(#)、チルダ(~)です。
-// デリミタを変更すれば、スラッシュをエスケープしないで正規表現内で使えます
 var_dump(preg_match('#^/path#', '/path/to/test'));
 var_dump(preg_match('~^/path~', '/path/to/test'));
