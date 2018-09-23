e6fcfb0af55e5f75c9ed2abf62684f329d19cdc8
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 17:55:58 +0900

    preg_quote

--- sample/ch10/02-preg-quote.php
+++ sample/ch10/02-preg-quote.php
@@ -3,17 +3,18 @@
  * preg_quoteによるメタ文字の置換
  */
 
-// 正規表現を想定していない、普通の文字列であるとする
+// 置換対象の文字列
 $str = 'aa*b*cc';
-// "a*b*c"という文字列を"d"にする構想
-[$word, $replace] = ["a*b*c","d"];
+// "a*b*c"という文字列を"d"にする構想です
+[$before, $after] = ["a*b*c","d"];
 
 // 結果がaa*b*ddになってしまう例
 // 理由は、アスタリスク(*)が正規表現におけるメタ文字だからです
-$result = preg_replace('/'.$word.'/', $replace, $str);
+$result = preg_replace('/'.$before.'/', $after, $str);
 echo $result.PHP_EOL;
 
 // メタ文字のアスタリスク(*)ではなく、単純な文字として判定させます
 // 結果：adc
-$result = preg_replace('/'.preg_quote($word, '/').'/', $replace, $str);
+$pregBefore = preg_quote($before, '/');
+$result = preg_replace('/'.$pregBefore.'/', $after, $str);
 echo $result.PHP_EOL;
