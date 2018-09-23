fdcfd7abc417bdbe2448ba49472480631640e863
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 22:52:07 +0900

    文字列の判定

---
+++ sample/ch12/02-if-string-zero.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 文字列のゼロ(0)の判定
+ */
+$val = "0";
+
+// 普通に判定するとヒットしない
+if ($val) {
+    echo "TRUE!".PHP_EOL;
+}
+// 文字列として考えて、長さを判定すればヒットする
+if (strlen($val) > 0) {
+    echo "strlen: TRUE!".PHP_EOL;
+}
