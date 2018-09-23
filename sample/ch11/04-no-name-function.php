d250dae299373237b204eacbce42931887b575ee
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 22:15:10 +0900

    -

--- sample/ch11/04-no-name-function.php
+++ sample/ch11/04-no-name-function.php
@@ -2,8 +2,6 @@
 /**
  * 無名関数が活躍する配列関数
  */
-$array = [1, 2, 3];
- 
 // 無名関数(クロージャ)は、配列関数で役に立ちます
 // 結果： [[0]=> int(2) [1]=> int(4) [2]=> int(6)]
-var_dump(array_map(function($v) { return $v * 2; }, $array));
+var_dump(array_map(function($v) { return $v * 2; }, [1, 2, 3]));
