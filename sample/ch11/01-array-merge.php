713226d4e13e0ba17f511bdc26e269d6b64861d2
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 21:15:41 +0900

    配列の結合

--- sample/ch11/01-array-merge.php
+++ sample/ch11/01-array-merge.php
@@ -3,9 +3,7 @@
  * 配列の結合
  */
 
-/**
- * 数値配列のケース
- */
+// 数値配列のケース
 $array1 = [1, 2];
 $array2 = [3, 4, 5];
 
@@ -14,15 +12,11 @@ $array2 = [3, 4, 5];
 var_dump($array1 + $array2);
 
 // array_mergeでは、数値配列は末尾に要素として追加されます
-// array(5) {
-//   [0]=> int(1) [1]=> int(2)
-//   [2]=> int(3) [3]=> int(4) [4]=> int(5)
-// }
+// array(5) { [0]=> int(1) [1]=> int(2)
+//   [2]=> int(3) [3]=> int(4) [4]=> int(5) }
 var_dump(array_merge($array1, $array2));
 
-/**
- * 連想配列のケース
- */
+// 連想配列のケース
 $array3 = ['fruits' => 'apple', 'code' => 'php'];
 $array4 = ['fruits' => 'oprange', 'programing' => 'perl'];
 
