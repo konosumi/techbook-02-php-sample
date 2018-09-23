4c08e88d975d2268213268fae309eb7b78ea7e28
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 13:51:41 +0900

    倍返しだ！

---
+++ sample/ch07/01-naoki-hanzawa.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Composerでインストールしたライブラリの利用
+ * SEE: https://packagist.org/
+ * require 'vendor/autoload.php';
+ */
+
+// 実用レベルでは、基盤・上流クラス側で予めrequireしておきます
+// 今回はサンプルなので、暫定的にディレクトリを上って読み込ます
+require implode(DIRECTORY_SEPARATOR,
+    [__DIR__, '..', '..', 'vendor', 'autoload.php']);
+
+// 2 * 倍返し = 4 
+echo "2 * 倍返し = ".NaokiHanzawa::baigaeshida(2).PHP_EOL;
+// 倍返しだ！倍返しだ！
+echo NaokiHanzawa::baigaeshida("倍返しだ！").PHP_EOL;
