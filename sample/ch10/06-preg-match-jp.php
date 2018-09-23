cc506aa91aaaf4348fa4a0cdc0a1eaddce4fce02
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-23 19:38:09 +0900

    日本語の正規表現の

--- sample/ch10/06-preg-match-jp.php
+++ sample/ch10/06-preg-match-jp.php
@@ -1,20 +1,19 @@
 <?php
-// パターン修飾子                                                                
-// SEE: http://php.net/manual/ja/reference.pcre.pattern.modifiers.php
+/**
+ * UTF-8 マルチバイト文字(日本語)の正規表現
+ */
 
-// u修飾子を付けないと、「あい」はバイト文字列のような扱いを受け、
-// 6文字分の変換がされてしまう
-// string(12) "雛鶴aaaaaa" 
-var_dump(preg_replace('/[あい]/', 'a', "雛鶴あい"));
-echo "\n<br>";
+ /**
+  * u修飾子を付けないと、「あい」はバイト文字列の扱いを受け、
+  * 6バイト(6文字)分の変換がされてしまいます
+  * 結果： "雛鶴aaaaaa" 
+  */
+echo preg_replace('/[あい]/', 'a', '雛鶴あい').PHP_EOL;
 
-// u (PCRE_UTF8)
-// この修正子は、Perl 非互換な PCRE の機能を有効にします。
-// パターンと対象文字列は、 UTF-8 として処理されます。 
-
-// 「あい」はそれぞれで1文字として処理されることで、想定した動きに直りました
-// string(8) "雛鶴aa" 
-var_dump(preg_replace('/[あい]/u', 'a', "雛鶴あい"));
-echo "\n<br>";
-
-var_dump(preg_replace('/[𡈽]/', 'a', "𡈽いちゃん"));
+/**
+ * u (PCRE_UTF8)
+ * => パターンと対象文字列は、 UTF-8として処理されます。 
+ * 「あい」はそれぞれで1文字として処理されます
+ * 結果： "雛鶴aa" 
+ */
+echo preg_replace('/[あい]/u', 'a', '雛鶴あい').PHP_EOL;
