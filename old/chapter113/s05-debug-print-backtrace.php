a52cee5d31eb90cc3d1eb9764ddf120fd1f41afd
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-24 10:04:48 +0900

    -

--- old/chapter113/s05-debug-print-backtrace.php
+++
@@ -1,7 +0,0 @@
-<?php
-function sample()
-{
-    debug_print_backtrace();
-}
-
-sample();
--- sample/ch13/04-set-error-handler.php
+++ sample/ch13/04-set-error-handler.php
@@ -2,13 +2,15 @@
 /**
  * エラーハンドラ
  */
+ini_set('display_errors', 1);
+// 全てのエラーが欲しい
 error_reporting(E_ALL);
 
 // 未定義の定数を使用すると、NOTICEレベルの例外が発生する
 // Notice: Use of undefined constant ABCDE - assumed 'ABCDE'
 ABCDE;
 
-// NOTICEレベルの例外も受信できます
+// エラーハンドラの登録(NOTICEレベルの例外も受信します)
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     echo 'errhandler: ', $errno, ' : ', $errstr, PHP_EOL;
 });
