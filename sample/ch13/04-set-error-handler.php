f8c2005e140419dcf35bd5ad328a87fbea5affed
konosumi <36559308+konosumi@users.noreply.github.com>
2018-09-24 09:44:24 +0900

    エラーハンドラ

--- sample/ch13/04-set-error-handler.php
+++ sample/ch13/04-set-error-handler.php
@@ -2,10 +2,17 @@
 /**
  * エラーハンドラ
  */
+error_reporting(E_ALL);
+
+// 未定義の定数を使用すると、NOTICEレベルの例外が発生する
+// Notice: Use of undefined constant ABCDE - assumed 'ABCDE'
+ABCDE;
+
+// NOTICEレベルの例外も受信できます
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-    echo 'ERROR: ', $errno, ' : ', $errstr, PHP_EOL;
+    echo 'errhandler: ', $errno, ' : ', $errstr, PHP_EOL;
 });
 
-// 未定義の定数を使用してみる
-// ERROR: 8 : Use of undefined constant ABCDE - assumed 'ABCDE'
+// 再び、未定義の定数を使用してみる
+// errhandler: 8 : Use of undefined constant ABCDE - assumed 'ABCDE'
 ABCDE;
